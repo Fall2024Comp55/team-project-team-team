590377f148a4dae1ed696a95fae13c12ca734565
Tristan Ramirez <t_ramirez4@u.pacific.edu>
2024-10-30 14:51:10 -0700

    SpeciesType enum

--- src/SpeciesType.java
+++ src/SpeciesType.java
@@ -1,7 +1,4 @@
 /**
- * This file has been implemented for you. You only need to use it.
- * Rather than declare a variable of type int or char, we will declare one of type
- * VehicleType when we want to store the type of Vehicle.
  * 
  * @author Osvaldo
  *
