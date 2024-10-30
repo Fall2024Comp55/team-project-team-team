1b6fb8d8495e1efe22c9ac492922c0df9f41f003
Tristan Ramirez <t_ramirez4@u.pacific.edu>
2024-10-30 14:49:49 -0700

    added speciesType enum

---
+++ src/SpeciesType.java
@@ -0,0 +1,21 @@
+/**
+ * This file has been implemented for you. You only need to use it.
+ * Rather than declare a variable of type int or char, we will declare one of type
+ * VehicleType when we want to store the type of Vehicle.
+ * 
+ * @author Osvaldo
+ *
+ */
+public enum SpeciesType{
+	MYCAR, TRUCK, AUTO;
+	
+	public String toString() {
+		switch(this) {
+			case MYCAR: return "car";
+			case TRUCK: return "truck";
+			case AUTO: return "auto";
+		}
+		return "n/a";
+	}
+	
+}
