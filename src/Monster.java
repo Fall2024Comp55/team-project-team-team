4f12157b551fad67a49729f025a2f56c9881367b
Nguyen Binh Ho <n_ho18@u.pacific.edu>
2024-11-02 15:05:44 -0700

    template for Monster class

---
+++ src/Monster.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import acm.graphics.*;
+
+public class Monster {
+	private SpeciesType species;
+    private GImage sprite;
+    private int experience;
+    private int level;
+    private int atk;
+    private int def;
+    private int spatk;
+    private int spdef;
+    private monsterType type1;
+    private monsterType type2;
+    private ArrayList<Moves> moves;
+    
+    
+    public boolean updateHP(int hpChange) {
+        //add code here
+        return false;
+    }
+
+    public void gainXP(int xp) {
+        //add code here
+    }
+
+    public void changeMove(int index, Move newMove) {
+        //add code here
+    }
+
+    public void levelUp() {
+        //add code here
+    }
+
+    public void evolve() {
+        //add code here
+    }
+}
