303801a6d02dc46179e59609fc13600448f86fe5
Tristan Ramirez <t_ramirez4@u.pacific.edu>
2024-11-03 19:33:38 -0800

    added getters and setters

--- src/Monster.java
+++ src/Monster.java
@@ -35,4 +35,93 @@ public class Monster {
     public void evolve() {
         //add code here
     }
+
+    /// getters and setters 
+	public SpeciesType getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(SpeciesType species) {
+		this.species = species;
+	}
+
+	public GImage getSprite() {
+		return sprite;
+	}
+
+	public void setSprite(GImage sprite) {
+		this.sprite = sprite;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+
+	public int getDef() {
+		return def;
+	}
+
+	public void setDef(int def) {
+		this.def = def;
+	}
+
+	public int getSpatk() {
+		return spatk;
+	}
+
+	public void setSpatk(int spatk) {
+		this.spatk = spatk;
+	}
+
+	public int getSpdef() {
+		return spdef;
+	}
+
+	public void setSpdef(int spdef) {
+		this.spdef = spdef;
+	}
+
+	public monsterType getType1() {
+		return type1;
+	}
+
+	public void setType1(monsterType type1) {
+		this.type1 = type1;
+	}
+
+	public monsterType getType2() {
+		return type2;
+	}
+
+	public void setType2(monsterType type2) {
+		this.type2 = type2;
+	}
+
+	public ArrayList<Moves> getMoves() {
+		return moves;
+	}
+
+	public void setMoves(ArrayList<Moves> moves) {
+		this.moves = moves;
+	}
 }
