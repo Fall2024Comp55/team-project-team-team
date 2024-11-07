c358d8831cbfaea1b1edb9724e468b8d3946d910
Melvin <m_boateng@u.pacific.edu>
2024-11-06 22:29:28 -0800

    Created trainer class and implemented setter and getter

---
+++ src/Trainer.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Trainer {
+	private ArrayList<Monster> team;
+	private int money;
+	
+	public ArrayList<Monster> getTeam(){
+		return team;
+	}
+	
+	public void setTeam(ArrayList<Monster> t) {
+		team = t;
+	}
+}
