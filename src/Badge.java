51087d238dfa77684a0762c8a8cf257d34febd74
Tristan Ramirez <t_ramirez4@u.pacific.edu>
2024-11-06 19:19:32 -0800

    Finished badge.java

--- src/Badge.java
+++ src/Badge.java
@@ -1,4 +1,36 @@
 
 public class Badge {
+	BadgeType badge;
+	boolean haveBadge;
+	String Description;
+	
+	Badge(BadgeType b){
+		badge = b;
+		haveBadge = false;
+		Description = b.toString();
+		
+	}
+	public String getDescription() {
+		return this.Description;
+	}
+	public void SetDescription(BadgeType d) {
+		this.Description = d.toString();
+	}
+	public void SetHaveBage(boolean b) {
+		this.haveBadge = b;
+	}
+	public boolean GetHaveBage() {
+		return this.haveBadge;
+	}
+	public void SetTypeBadge(BadgeType b) {
+		this.badge = b;
+	}
+	public BadgeType GetBadgeType() {
+		return this.badge;
+	}
+	
 
+	
+	
+	
 }
