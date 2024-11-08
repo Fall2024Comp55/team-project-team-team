db9ffb851cbda7183c9428b4f08f0bb7b273ff9e
Tristan Ramirez <t_ramirez4@u.pacific.edu>
2024-11-08 14:32:05 -0800

    Added  trainerSetTeam and  trainergetTeam function to player trainer

--- src/PlayerTrainer.java
+++ src/PlayerTrainer.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 
 public class PlayerTrainer {
+	private Trainer trainer;
 	private int numBadges;
 	private int currMoney;
 	private Bag bag;
@@ -8,8 +10,19 @@ public class PlayerTrainer {
 		currMoney = currMoney + amount;
 	}
 	
+	public void updatenumBadges(int numBadge) {
+		this.numBadges = this.numBadges + numBadge;
+	}
+	
 	public void openBag() {
 		bag = new Bag();
+	}
+	
+	public void trainerSetTeam(ArrayList<Monster> t){
+		this.trainer.setTeam(t);
+	}
+	public ArrayList<Monster> trainergetTeam(){
+		return (this.trainer.getTeam());
 	}
 	
 	
