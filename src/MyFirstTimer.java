584a54a8b34318eeb9aa6322d3f931d57a67f970
Melvin <m_boateng@u.pacific.edu>
2024-11-11 14:11:53 -0800

    added Direction checker

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -13,15 +13,16 @@ import acm.program.GraphicsProgram;
 
 
 public class MyFirstTimer extends GraphicsProgram implements ActionListener, KeyListener{
-	public static final int PROGRAM_HEIGHT = 300;
-	public static final int PROGRAM_WIDTH = 400;
+	public static final int PROGRAM_HEIGHT = 600;
+	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private int numTimes ;
 	public Timer timer = new Timer(1000, this);
 	private GLabel myLabel;
 	private GImage userPlayer = new GImage("media/user2BackProfile.png");
 	private GImage grassBackground = new GImage("media/grassBackground.jpg");
-	private int labelX =400, labelY = 300;
+	private int labelX =0, labelY = 0;
+	private PlayerTrainer userP = new PlayerTrainer();
 	
 
 	public void actionPerformed(ActionEvent e) {
@@ -39,7 +40,7 @@ public class MyFirstTimer extends GraphicsProgram implements ActionListener, Key
 		userPlayer.scale(0.2);
 		addKeyListeners();
 		requestFocus();
-		userPlayer.setLocation(900, 700);
+		userPlayer.setLocation(450, 200);
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
@@ -48,18 +49,26 @@ public class MyFirstTimer extends GraphicsProgram implements ActionListener, Key
 		switch(keyCode) {
 		case KeyEvent.VK_W://up
 			labelY += 10;
+			userP.setDirection(Direction.UP);
+			System.out.println(userP.getDirection());
 			System.out.println("Moving Up");
 			break;
 		case KeyEvent.VK_S://down
 			labelY -= 10;
+			userP.setDirection(Direction.DOWN);
+			System.out.println(userP.getDirection());
 			System.out.println("Moving Down");
 			break;
 		case KeyEvent.VK_A://left
 			labelX += 10;
+			userP.setDirection(Direction.LEFT);
+			System.out.println(userP.getDirection());
 			System.out.println("Moving Left");
 			break;
 		case KeyEvent.VK_D://right
 			labelX -= 10;
+			userP.setDirection(Direction.RIGHT);
+			System.out.println(userP.getDirection());
 			System.out.println("Moving Right");
 			break;
 		}
