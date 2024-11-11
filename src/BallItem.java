61f1a1045a5434494ffbf7f7ea6fdaf621a356e7
Nguyen Binh Ho <n_ho18@u.pacific.edu>
2024-11-11 15:16:46 -0800

    add comments to explain the code

--- src/BallItem.java
+++ src/BallItem.java
@@ -6,7 +6,11 @@ public class BallItem {
     public void Use() {
         if(catchRate > 0) {
         	Random rand = new Random();
+        	//regenerate random integer number from 0 to 100
         	int rate = rand.nextInt(101);
+        	//if user uses ball with 25% catch rate, I assume that the user has 25 out 100 chance to catch the monster
+        	//calculate 100 - 25 = 75
+        	//If the rate is greater or equals to 75 (75 <= rate <= 100) (let says 80), the user fall into 1 in 25 successful cases
         	if(rate >= 100 - catchRate) System.out.println("You have successfully caught the monster.");
         	else System.out.println("You failed to catch the monster.");
         }
