2aac44d350b703da908c8a4b29a4288f8f340b67
Nguyen Binh Ho <n_ho18@u.pacific.edu>
2024-11-11 15:08:44 -0800

    add Shop class

---
+++ src/Shop.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+public class Shop {
+    private Map<String, Integer> itemList;
+    
+    public void displayMenu() {
+    	//add code here
+    }
+    
+    public void buy(String name, int quantity) {
+    	
+    }
+    
+    public int sell() {
+    	//add code here
+    	return 0;
+    }
+}
