83c077270a6b640cbe541d499d2dcb5f05f636ef
Nguyen Binh Ho <n_ho18@u.pacific.edu>
2024-11-11 15:05:43 -0800

    change catchRate type to integer and range of random number to 0 to 100

--- src/BallItem.java
+++ src/BallItem.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 
 public class BallItem {
-	private double catchRate;
+	private int catchRate;
     
     public void Use() {
         if(catchRate > 0) {
         	Random rand = new Random();
-        	double rate = rand.nextDouble();
-        	if(rate >= 1.0 - catchRate) System.out.println("You have successfully caught the monster.");
+        	int rate = rand.nextInt(101);
+        	if(rate >= 100 - catchRate) System.out.println("You have successfully caught the monster.");
         	else System.out.println("You failed to catch the monster.");
         }
     }
@@ -16,16 +16,16 @@ public class BallItem {
     	//For now, I assume i is the index for the ball type.
     	switch(i) {
             case 1:
-                this.catchRate = 0.25;
+                this.catchRate = 25;
                 break;
             case 2:
-                this.catchRate = 0.5;
+                this.catchRate = 50;
                 break;
             case 3:
-                this.catchRate = 0.75;
+                this.catchRate = 75;
                 break;
             case 4:
-                this.catchRate = 1.0;
+                this.catchRate = 100;
                 break;
             default:
                 System.out.println("Invalid ball");
