62a6a4de87f0fc801e8b7433f6abe457c76d7e00
rhavlan <rhavlan@STK-ECS-JR4S243.stk.pacific.edu>
2024-11-13 14:19:22 -0800

    Added placeholder tile images

--- src/Space.java
+++ src/Space.java
@@ -1,12 +1,28 @@
 import acm.graphics.*;
 
-public class Space {
+enum Space {
+	PATH(true, "Path.png"),
+	GRASS(true, "Grass.png"),
+	TALLGRASS(true, "Tall_Grass.png"),
+	OBSTACLE(false, "Obstacle.png");
+	
+	Space(boolean walkable, String tile) {
+		this.walkable = walkable;
+		this.tile = new GImage(tile);
+	}
+	
 	public boolean walkable;
-	private String type;
 	private GImage tile;
 	
-	
-	public void drawSpace() {
+	public void drawSpace(int x, int y) {
 		// TODO draw the space on the screen in the right location
+	}
+	
+	public void setPos(int x, int y) {
+		tile.setLocation(x, y);
+	}
+	
+	public void move(int dx, int dy) {
+		tile.move(dx, dy);
 	}
 }
