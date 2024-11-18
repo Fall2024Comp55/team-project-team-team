41583c4e87365026190c8ece475bb81f9d4033e4
Tristan Ramirez <t_ramirez4@u.pacific.edu>
2024-11-17 21:47:53 -0800

    Finished the healing center, added dialouge and healing class

--- src/HealingCenter.java
+++ src/HealingCenter.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 	
 public class HealingCenter extends PlayerTrainer {
- 
+	private String dialogue1 = "Hi i woulld you like to heal you monsters";
+	private String dialogue2 = "You choose to heal";
+	private String dialogue3 = "You choose not to heal";
+	private String dialogue4 = "Thank you for stoping buy have a nice day";
+	private boolean T_or_F = false;
+		
 	
 	public void healing( PlayerTrainer p  ) {
 		ArrayList<Monster> Monsters = p.getTeam(); 
@@ -12,9 +17,17 @@ public class HealingCenter extends PlayerTrainer {
 		}
 	 }
 	
-	
-	    
-	   
+
+	public void Healing_dialogue( PlayerTrainer p  ) {
+		System.out.println(dialogue1);
+		if(T_or_F ) {
+			System.out.println(dialogue2);
+			healing(p);
+		}else {
+			System.out.println(dialogue3);
+		}
+		System.out.println(dialogue4);
+	 }
 	
 
 }
