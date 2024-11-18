2b72466fb07bf22242f4e51fa28e42682c23df33
Reuben <r_havlan@u.pacific.edu>
2024-11-17 22:59:51 -0800

    Progress on movement logic. Added 16x16 placeholder character sprite

--- src/Map.java
+++ src/Map.java
@@ -9,17 +9,23 @@ import javax.swing.Timer;
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
-
+// comment
 
 public class Map extends GraphicsProgram implements ActionListener, KeyListener{
-	public static final int PROGRAM_HEIGHT = 600;
-	public static final int PROGRAM_WIDTH = 800;
+	public static final int SCREEN_TILES_WIDTH = 5;
+	public static final int SCREEN_TILES_HEIGHT = 5;
 	public static final int MAX_STEPS = 20;
-	public static final double SCALE_FACTOR = 5.00;
-	private int numTimes;
+	public static final int TILE_RESOLUTION = 16;
+	public static final int SCALE_FACTOR = 5;
+	
+	private int tileSize     = TILE_RESOLUTION * SCALE_FACTOR;
+	private int screenWidth  = tileSize * SCREEN_TILES_WIDTH;
+	private int screenHeight = tileSize * SCREEN_TILES_HEIGHT;
+	
+	private int numTimes ;
 	public Timer timer = new Timer(1000, this);
 	private GLabel myLabel;
-	private GImage userPlayer = new GImage("media/user2BackProfile.png");
+	private GImage userPlayer = new GImage("media/placeholderCharacter.png");
 	private GImage grassBackground = new GImage("media/grassBackground.jpg");
 	private int labelX =0, labelY = 0;
 	private PlayerTrainer userP = new PlayerTrainer();
@@ -29,6 +35,10 @@ public class Map extends GraphicsProgram implements ActionListener, KeyListener{
 	private ArrayList<GImage> tiles = new ArrayList<GImage>();
 	private GImage nextTile;
 	
+	private boolean movable = true;
+	private int playerXOffset = 0;
+	private int playerYOffset = 0;
+	
 	
 	public void actionPerformed(ActionEvent e) {
 		
@@ -36,7 +46,7 @@ public class Map extends GraphicsProgram implements ActionListener, KeyListener{
 	
 	
 	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+		setSize(tileSize * SCREEN_TILES_WIDTH, tileSize * SCREEN_TILES_HEIGHT);
 		int count = 0;
 		for(Space[] x : map.spaceMap) {
 			System.out.println("adding line");
@@ -52,7 +62,19 @@ public class Map extends GraphicsProgram implements ActionListener, KeyListener{
 			nextPos.translate(-16 * count * SCALE_FACTOR, 16 * SCALE_FACTOR);
 			count = 0;
 		}
+		add(userPlayer);
+		userPlayer.scale(SCALE_FACTOR);
+		userPlayer.setLocation(tileSize * map.startX, tileSize * map.startY);
+		moveBackground(-1 * tileSize * (map.startX - (int)(SCREEN_TILES_WIDTH/2)), -1 * tileSize * (map.startY - SCREEN_TILES_HEIGHT + 1));
+		movePlayer(-1 * tileSize * (map.startX - (int)(SCREEN_TILES_WIDTH/2)), -1 * tileSize * (map.startY - SCREEN_TILES_HEIGHT + 1));
+		playerXOffset = (screenWidth/2 - (int)userPlayer.getX())/tileSize;
+		playerYOffset = (screenHeight/2 - (int)userPlayer.getY())/tileSize - 1;
+		System.out.println(playerXOffset);
+		System.out.println(playerYOffset);
 		
+		
+		addKeyListeners();
+		requestFocus();
 		/*
 		add(grassBackground);
 		grassBackground.scale(2);
@@ -60,56 +82,109 @@ public class Map extends GraphicsProgram implements ActionListener, KeyListener{
 		grassBackground.setLocation(labelX, labelY);
 		userPlayer.setLocation(labelX, labelY);
 		userPlayer.scale(0.2);
-		addKeyListeners();
-		requestFocus();
 		userPlayer.setLocation(450, 200);
 		*/
+	}
+	
+	public void moveBackground(int x, int y) {
+		for(GImage z : tiles) {
+			z.move(x, y);
+		}
+	}
+	
+	public void movePlayer(int x, int y) {
+		userPlayer.move(x, y);
 	}
 	
 	public void move(Direction direction) {
 		switch(direction) {
 		case UP:
-			for(int i = 0; i < 16; i++) {
-				
+			if(playerYOffset < 0 || tiles.getFirst().getY() >= 0) {
+				for(int i = 0; i < TILE_RESOLUTION; i++) {
+					movePlayer(0, -SCALE_FACTOR);
+				}
+				playerYOffset += 1;
+			} else {
+				for(int i = 0; i < TILE_RESOLUTION; i++) {
+					moveBackground(0, SCALE_FACTOR);
+				}
 			}
+			break;
 		case DOWN:
-			
+			if(playerYOffset > 0 || tiles.getLast().getY() <= screenHeight - tileSize) {
+				for(int i = 0; i < TILE_RESOLUTION; i++) {
+					movePlayer(0, SCALE_FACTOR);
+				}
+				playerYOffset -= 1;
+			} else {
+				for(int i = 0; i < TILE_RESOLUTION; i++) {
+					moveBackground(0, -SCALE_FACTOR);
+				}
+			}
+			break;
 		case LEFT:
-			
+			if(playerXOffset > 0 || tiles.getFirst().getX() >= 0) {
+				for(int i = 0; i < TILE_RESOLUTION; i++) {
+					movePlayer(-SCALE_FACTOR, 0);
+				}
+				playerXOffset -= 1;
+			} else {
+				for(int i = 0; i < TILE_RESOLUTION; i++) {
+					moveBackground(SCALE_FACTOR, 0);
+				}
+			}
+			break;
 		case RIGHT:
-			
+			if(playerXOffset < 0 || tiles.getLast().getX() <= screenWidth - tileSize) {
+				for(int i = 0; i < TILE_RESOLUTION; i++) {
+					movePlayer(SCALE_FACTOR, 0);
+				}
+				playerXOffset += 1;
+			} else {
+				for(int i = 0; i < TILE_RESOLUTION; i++) {
+					moveBackground(-SCALE_FACTOR, 0);
+				}
+			}
+			break;
 		}
+		movable = true;
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
-		int keyCode = e.getKeyCode();
-		
-		switch(keyCode) {
-		case KeyEvent.VK_W://up
-			move(Direction.UP);
-			userP.setDirection(Direction.UP);
-			System.out.println(userP.getDirection());
-			System.out.println("Moving Up");
-			break;
-		case KeyEvent.VK_S://down
-			userP.setDirection(Direction.DOWN);
-			System.out.println(userP.getDirection());
-			System.out.println("Moving Down");
-			break;
-		case KeyEvent.VK_A://left
-			userP.setDirection(Direction.LEFT);
-			System.out.println(userP.getDirection());
-			System.out.println("Moving Left");
-			break;
-		case KeyEvent.VK_D://right
-			userP.setDirection(Direction.RIGHT);
-			System.out.println(userP.getDirection());
-			System.out.println("Moving Right");
-			break;
+		if(movable) {
+			movable = false;
+			int keyCode = e.getKeyCode();
+			
+			switch(keyCode) {
+			case KeyEvent.VK_W://up
+				move(Direction.UP);
+				userP.setDirection(Direction.UP);
+				System.out.println(userP.getDirection());
+				System.out.println("Moving Up");
+				break;
+			case KeyEvent.VK_S://down
+				move(Direction.DOWN);
+				userP.setDirection(Direction.DOWN);
+				System.out.println(userP.getDirection());
+				System.out.println("Moving Down");
+				break;
+			case KeyEvent.VK_A://left
+				move(Direction.LEFT);
+				userP.setDirection(Direction.LEFT);
+				System.out.println(userP.getDirection());
+				System.out.println("Moving Left");
+				break;
+			case KeyEvent.VK_D://right
+				move(Direction.RIGHT);
+				userP.setDirection(Direction.RIGHT);
+				System.out.println(userP.getDirection());
+				System.out.println("Moving Right");
+				break;
+			}
+			// grassBackground.setLocation(labelX, labelY);
+			movable = true;
 		}
-		grassBackground.setLocation(labelX, labelY);
-		
 	}
 	
 	public void run() {
--- src/Maps.java
+++ src/Maps.java
@@ -10,11 +10,15 @@ public enum Maps {
 		{Space.OBSTACLE,Space.GRASS,Space.GRASS,Space.PATH,Space.GRASS,Space.GRASS,Space.OBSTACLE},
 		{Space.OBSTACLE,Space.GRASS,Space.GRASS,Space.PATH,Space.GRASS,Space.GRASS,Space.OBSTACLE},
 		{Space.OBSTACLE,Space.OBSTACLE,Space.OBSTACLE,Space.PATH,Space.OBSTACLE,Space.OBSTACLE,Space.OBSTACLE}
-	});
+	}, 3, 8);
 	
-	Maps(Space[][] spaceMap) {
+	Maps(Space[][] spaceMap, int x, int y) {
 		this.spaceMap = spaceMap;
+		this.startX = x;
+		this.startY = y;
 	}
 	
 	public Space[][] spaceMap;
+	public int startX;
+	public int startY;
 }
