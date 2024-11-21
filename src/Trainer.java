124c4def3c890eb7c56604953466f012b580ac50
Tristan Ramirez <t_ramirez4@u.pacific.edu>
2024-11-20 20:37:17 -0800

    Added randomTeam methons for trainer

--- src/Trainer.java
+++ src/Trainer.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 //test2
 public class Trainer {
 	private ArrayList<Monster> team;
@@ -7,7 +8,7 @@ public class Trainer {
 	
 	
 	public Trainer() {
-		team = new ArrayList<>();
+		team = new ArrayList<Monster>();
 		money = 0;
 	}
 	
@@ -19,6 +20,19 @@ public class Trainer {
 		team = t;
 	}
 	
+	public void addMon(Monster m) {
+		 team.add(m);
+	}
+	
+	public Monster switchMon(int num) {
+        if (num >= 0 && num < team.size()) {
+            return team.get(num); // Return the monster at the specified index
+        } else {
+            System.out.println("Invalid index for switching monsters.");
+            return null;
+        }
+    }
+	
 	public int getMoney() {
 		return money;
 	}
@@ -27,5 +41,19 @@ public class Trainer {
 		money = amount;
 	}
 	
+	public void setRandomTeam() {
+	    Random rand = new Random();
+	    
+	    SpeciesType[] availableSpecies = SpeciesType.values();
+
+	    for (int i = 0; i < 4; i++) {
+	        SpeciesType species = availableSpecies[rand.nextInt(availableSpecies.length)];
+
+	        Monster randomMonster = new Monster(species); // Assuming Monster constructor takes SpeciesType
+
+	        team.add(randomMonster);
+	    }
+	}
 	
+		
 }
