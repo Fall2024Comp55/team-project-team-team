ff85737a4716dbf2dd237b52b333c2cd53096439
havla <havla@10.15.177.10>
2024-12-03 09:07:20 -0800

    printMap is now createMap is Map.java

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,4 @@
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
@@ -11,7 +10,7 @@ import acm.program.GraphicsProgram;
 
 
 
-public class Map extends GraphicsProgram implements ActionListener, KeyListener {
+public class Map extends GraphicsProgram implements KeyListener {
 	public static final int SCREEN_TILES_WIDTH = 11;
 	public static final int SCREEN_TILES_HEIGHT = 9;
 	public static final int MAX_STEPS = 20;
